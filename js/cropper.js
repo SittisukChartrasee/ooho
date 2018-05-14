621d029393d12ced6a799f3c6d0c2805f6b41fd5
sittisuk <beerxp1@gmail.com>
2018-05-14 10:30:52 +0700

    update cropper

--- js/cropper.js
+++ js/cropper.js
@@ -3,12 +3,15 @@ var canvas  = $("#canvas"),
         $result = $('#result');
         $picture = $('#picture');
         $img = $('#img');
+        var croppedImageDataURL = null
     
     $('#fileInput').on( 'change', function(){
       $(".modal").addClass("is-active");
         if (this.files && this.files[0]) {
+          console.log(this.files, this.files[0])
           if ( this.files[0].type.match(/^image\//) ) {
             var reader = new FileReader();
+            console.log('reader', reader)
             reader.onload = function(evt) {
                var img = new Image();
                img.onload = function() {
@@ -23,18 +26,19 @@ var canvas  = $("#canvas"),
                  });
                  $('#btnCrop').click(function() {
                     // Get a string base 64 data url
-                    var croppedImageDataURL = canvas.cropper('getCroppedCanvas').toDataURL("image/png");
-                    $picture.html(`<img style="width: 100%;" src="${croppedImageDataURL}" />`)
+                    croppedImageDataURL = canvas.cropper('getCroppedCanvas').toDataURL("image/png");
+                    // $picture.html(`<img style="width: 100%;" src="${croppedImageDataURL}" />`)
+                    $picture.html( $('<img>').attr('src', croppedImageDataURL).attr('style', 'width: 300px') );                    
                     $img.html(`<img style="margin: 0 auto;position: relative;overflow: hidden;width: 100%;border-radius: 10px;" src="../../images/blog/register.png" />`)
                     $('.bg-image').remove()
                     // $result.html($('<img>').attr('src', croppedImageDataURL))
-                    // $result.append( $('<img>').attr('src', croppedImageDataURL) );
+                    $picture.append( $('<img>').attr('src', croppedImageDataURL) );
                     // console.log($picture)
                     $(".modal").removeClass("is-active");
                  });
                  $('#btnRestore').click(function() {
-                   canvas.cropper('reset');
-                   $result.empty();
+                  canvas.cropper('reset');
+                  $picture.empty();
                  });
                };
                img.src = evt.target.result || ev.dataTransfer.files;
@@ -47,6 +51,9 @@ var canvas  = $("#canvas"),
         }
         else {
           alert('No file(s) selected.');
+          canvas.cropper('reset');          
+          $picture.html(`<img style="width: 100%;" src="${croppedImageDataURL}" />`)
+          $(".modal").removeClass("is-active");
         }
     });
     
