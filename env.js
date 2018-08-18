ab83200b8d32e937fd8ad886349755c9b43fd1b3
Sittisuk <sittisuk@codefin.io>
2018-08-18 14:10:26 +0700

    change end point

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 window.env = {
   AUTH0_DOMAIN: 'sittisuk.auth0.com',
-  AUTH0_CLIENT_ID: 'AR6mzvSkwz1TMi5ZzMZB2lvdmvj1xX2z',
+  AUTH0_CLIENT_ID: '7sXTRrDcRwXHb8yGtswg8MlJbxzddkZU',
   AUTH0_CALLBACK_URL: 'http://localhost:3000/login.html',
-  AUTH0_CALLBACK_URL_PRODUCTION: 'https://SittisukChartrasee.github.io/ooho/login.html',
+  AUTH0_CALLBACK_URL_PRODUCTION: 'https://sittisukchartrasee.github.io/ooho/login.html',
 };
